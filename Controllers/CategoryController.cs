7a8be47f3a2bb2e0aab47fee8284e118072eb620
lovefamilyviet203 <huyviet181@gmail.com>
2024-07-23 01:00:47 +0700

    xong het category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,5 +16,78 @@ namespace ProjectEcommerce.Controllers
             List<CategoryModel> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CategoryModel obj)
+        {
+            if (obj.CategoryName == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("categoryname", "The DisplayOrder cannot exactly match the Category Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            CategoryModel? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(CategoryModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            CategoryModel? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            CategoryModel? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectEcommerce.Models
 {
@@ -7,9 +8,15 @@ namespace ProjectEcommerce.Models
         [Key]
         public int CategoeyID { get; set; }
         [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
         public string CategoryName { get; set; }
         [Required]
+        [MaxLength(3000)]
+        [DisplayName("Category Discription")]
         public string CategoryDescription { get; set; }
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 - 100")]
+        [DisplayName("Display Order")]
         public int DisplayOrder {  get; set; }
     }
 }
