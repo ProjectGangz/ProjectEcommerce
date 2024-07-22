03eeb3c6e3e6f7d9690ba803fbab52461f03bdee
lovefamilyviet203 <huyviet181@gmail.com>
2024-07-22 14:11:02 +0700

    category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectEcommerce.Data;
+using ProjectEcommerce.Models;
+
+namespace ProjectEcommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<CategoryModel> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectEcommerce.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int CategoeyID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public string CategoryDescription { get; set; }
+        public int DisplayOrder {  get; set; }
+    }
+}
